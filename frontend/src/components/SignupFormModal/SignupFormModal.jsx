067caca3bb4950be7b6681b38f6f26b9176bad18
cpeter41 <chris.steven.peters@gmail.com>
2024-06-03 10:59:09 -0700

    signup form basics

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -1,46 +1,37 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import * as sessionActions from "../../store/session";
 import "./SignupForm.css";
 import "../LoginFormModal/LoginForm.css";
 
+const PROFILE_ICONS_LEN = 12;
+const profileIcons = [];
+for (let i = 1; i <= PROFILE_ICONS_LEN; i++) profileIcons.push(i);
+
 function SignupFormModal() {
     const dispatch = useDispatch();
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
+    const [imageId, setImageId] = useState(
+        Math.ceil(Math.random() * PROFILE_ICONS_LEN)
+    );
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [errors, setErrors] = useState({});
-    const [disabled, setDisabled] = useState(true);
     const { closeModal } = useModal();
-
-    useEffect(() => {
-        if (
-            email.length &&
-            username.length >= 4 &&
-            firstName.length &&
-            lastName.length &&
-            password.length >= 6 &&
-            confirmPassword.length
-        )
-            setDisabled(false);
-        else setDisabled(true);
-    }, [email, username, firstName, lastName, password, confirmPassword]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (password === confirmPassword) {
             setErrors({});
+            console.log(imageId);
             return dispatch(
                 sessionActions.signup({
                     email,
                     username,
-                    firstName,
-                    lastName,
                     password,
+                    imageId
                 })
             )
                 .then(closeModal)
@@ -51,65 +42,71 @@ function SignupFormModal() {
                     }
                 });
         }
-        return setErrors({
-            confirmPassword:
-                "Confirm Password field must be the same as the Password field",
-        });
+        return setErrors({ confirmPassword: "Passwords do not match." });
     };
 
     return (
         <div className="flex-col" id="signup-container">
             <h1>Sign Up</h1>
-            <form onSubmit={handleSubmit} className="flex-col" id="login-form">
-                <label>Email</label>
-                <input
-                    type="text"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Email"
-                />
-                {errors.email && <p>Please enter a valid email</p>}
-                <label>Username</label>
-                <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder="Username"
-                />
-                {errors.username && <p>{errors.username}</p>}
-                <label>First Name</label>
-                <input
-                    type="text"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    placeholder="First Name"
-                />
-                {errors.firstName && <p>{errors.firstName}</p>}
-                <label>Last Name</label>
-                <input
-                    type="text"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    placeholder="Last Name"
-                />
-                {errors.lastName && <p>{errors.lastName}</p>}
-                <label>Password</label>
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
-                />
-                {errors.password && <p>{errors.password}</p>}
-                <label>Confirm Password</label>
-                <input
-                    type="password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    placeholder="Confirm Password"
-                />
-                {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-                <button type="submit" id="submit" disabled={disabled}>
+            <form onSubmit={handleSubmit} className="flex-col" id="signup-form">
+                <div className="signup-div">
+                    <label>Email</label>
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email"
+                    />
+                    <span>{errors.email}</span>
+                </div>
+                <div className="signup-div">
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Username"
+                    />
+                    <span>{errors.username}</span>
+                </div>
+                <div className="signup-div">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                    />
+                    <span>{errors.password}</span>
+                </div>
+                <div className="signup-div">
+                    <label>Confirm Password</label>
+                    <input
+                        type="password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        placeholder="Confirm Password"
+                    />
+                    <span>{errors.confirmPassword}</span>
+                </div>
+                <div id="profile-icons">
+                    {profileIcons.map((iconId) => (
+                        <label key={iconId}>
+                            <input
+                                type="radio"
+                                name="profile-icon-sel"
+                                id={`profileIcon${iconId}`}
+                                value={iconId}
+                                checked={imageId === iconId}
+                                onChange={(e) => {
+                                    setImageId(parseInt(e.target.value));
+                                }}
+                            />
+                            icon {iconId}
+                        </label>
+                    ))}
+                </div>
+                <button type="submit" id="submit">
                     Sign Up
                 </button>
             </form>
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -32,15 +32,14 @@ export const restoreUser = () => async (dispatch) => {
 };
 
 export const signup = (user) => async (dispatch) => {
-    const { username, firstName, lastName, email, password } = user;
+    const { username, email, password, imageId } = user;
     const response = await csrfFetch("/api/users", {
         method: "POST",
         body: JSON.stringify({
             username,
-            firstName,
-            lastName,
             email,
             password,
+            imageId
         }),
     });
     const data = await response.json();
