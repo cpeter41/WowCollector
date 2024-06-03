ef8788d2bc34dde2c6ba2b29683c18e44ebd7049
cpeter41 <chris.steven.peters@gmail.com>
2024-06-02 20:03:32 -0700

    login utils

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,31 @@
 const router = require("express").Router();
+const {
+    // setTokenCookie,
+    restoreUser,
+    // requireAuth,
+} = require("../../utils/auth.js");
+// const { User } = require("../../db/models");
+
+router.use(restoreUser);
+
+// router.get("/restore-user", (req, res) => {
+//     return res.json(req.user);
+// });
+
+// // GET /api/set-token-cookie
+// router.get("/set-token-cookie", async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: "DemoUser",
+//         },
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user: user });
+// });
+
+// router.get("/require-auth", requireAuth, (req, res) => {
+//     return res.json(req.user);
+// });
 
 router.get("/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
