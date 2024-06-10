3dd494c120c97e694eae0d67df6688be37baa248
cpeter41 <chris.steven.peters@gmail.com>
2024-06-10 03:23:27 -0700

    stuff

--- frontend/src/components/Modals/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/Modals/SignupFormModal/SignupFormModal.jsx
@@ -4,12 +4,23 @@ import { useModal } from "../../../context/Modal";
 import * as sessionActions from "../../../redux/session";
 import "./SignupForm.css";
 import "../LoginFormModal/LoginForm.css";
+import aws from "aws-sdk";
 
 const PROFILE_ICONS_LEN = 12;
 const profileIcons = [];
 for (let i = 1; i <= PROFILE_ICONS_LEN; i++) profileIcons.push(i);
 
 function SignupFormModal() {
+    console.log(import.meta.env.VITE_AWS_ACCESS_KEY_ID);
+    aws.config.update({
+        accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+        secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+    });
+    const s3 = new aws.S3({
+        params: { Bucket: import.meta.env.VITE_S3_BUCKET },
+        region: import.meta.env.VITE_AWS_REGION,
+    });
+    // console.log(s3);
     const dispatch = useDispatch();
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
@@ -18,11 +29,28 @@ function SignupFormModal() {
     );
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    // consider setting imageurl default to something from aws
+    const [imageFile, setImageFile] = useState(null);
+    const [imageUrl, setImageUrl] = useState("");
     const [errors, setErrors] = useState({});
     const { closeModal } = useModal();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        // let formData = new FormData();
+        // formData.append("file", imageFile);
+        const params = {
+            Bucket: import.meta.env.VITE_S3_BUCKET,
+            Key: imageFile.name,
+            Body: imageFile
+        }
+
+        const upload = s3.putObject(params).promise();
+        await upload.then((e, data) => {
+            console.log(e);
+            console.log("done");
+        })
+
         if (password === confirmPassword) {
             setErrors({});
             return dispatch(
@@ -30,7 +58,7 @@ function SignupFormModal() {
                     email,
                     username,
                     password,
-                    imageId
+                    imageId,
                 })
             )
                 .then(closeModal)
@@ -42,6 +70,15 @@ function SignupFormModal() {
                 });
         }
         return setErrors({ confirmPassword: "Passwords do not match." });
+    };
+
+    const handleImageChange = (e) => {
+        e.stopPropagation();
+
+        const file = e.target.files[0];
+        setImageFile(file);
+
+        setImageUrl(URL.createObjectURL(file));
     };
 
     return (
@@ -88,7 +125,25 @@ function SignupFormModal() {
                     />
                     <span>{errors.confirmPassword}</span>
                 </div>
-                <div id="profile-icons">
+                <div id="image-upload">
+                    <div id="choose-image">
+                        <label htmlFor="image-input">
+                            Choose a Profile Image
+                        </label>
+                        <input
+                            id="image-input"
+                            type="file"
+                            accept="image/png, image/jpeg, image/jpg"
+                            onChange={handleImageChange}
+                        ></input>
+                    </div>
+                    <div id="preview-image">
+                        {imageUrl !== "" && (
+                            <img src={imageUrl} alt="profile-picture" />
+                        )}
+                    </div>
+                </div>
+                {/* <div id="profile-icons">
                     {profileIcons.map((iconId) => (
                         <label key={iconId}>
                             <input
@@ -104,8 +159,8 @@ function SignupFormModal() {
                             icon {iconId}
                         </label>
                     ))}
-                </div>
-                <button type="submit" id="submit">
+                </div> */}
+                <button type="submit" id="signup-submit">
                     Sign Up
                 </button>
             </form>
