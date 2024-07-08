6632f704e37b495f43855357f55dab7f8e094083
cpeter41 <chris.steven.peters@gmail.com>
2024-07-07 23:23:47 -0700

    title seed fix

--- backend/db/migrations/20240704073229-create-title.js
+++ backend/db/migrations/20240704073229-create-title.js
@@ -1,4 +1,10 @@
 "use strict";
+
+let options = { validate: true };
+if (process.env.NODE_ENV === "production") {
+    options.schema = process.env.SCHEMA;
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
@@ -35,9 +41,9 @@ module.exports = {
                 type: Sequelize.DATE,
                 defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
             },
-        });
+        }, options);
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable("Titles");
+        await queryInterface.dropTable("Titles", options);
     },
 };
